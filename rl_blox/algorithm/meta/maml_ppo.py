44949f42db3cb053ddf4993f04f508aa53e11e0a
Sven Mandic <51965596+smandic5@users.noreply.github.com>
2025-11-12 10:23:11 +0100

    Maml task set and logging

--- rl_blox/algorithm/meta/maml_ppo.py
+++ rl_blox/algorithm/meta/maml_ppo.py
@@ -12,7 +12,8 @@ from tqdm.rich import trange
 
 from ...blox.function_approximator.policy_head import StochasticPolicyBase
 from ...blox.gae import compute_gae
-from ...logging.logger import LoggerBase
+from ...blox.multitask import TaskSelector
+from ...logging.logger import LoggerBase, MemoryLogger
 from ..ppo import collect_trajectories, ppo_loss, update_ppo
 
 
@@ -21,6 +22,7 @@ def inner_loop(
     actor: StochasticPolicyBase,
     critic: nnx.Module,
     key: jnp.ndarray,
+    current_iteration: int,
     batch_size: int = 64,
     epochs: int = 1,
     inner_actor_lr: float = 0.001,
@@ -30,6 +32,7 @@ def inner_loop(
     update_ppo_jitted = nnx.jit(update_ppo, static_argnames="epochs")
 
     # collect trajectory
+    logger_adapting = logger if logger is None else MemoryLogger()
     key, subkey = jax.random.split(key)
     (
         observation,
@@ -39,7 +42,15 @@ def inner_loop(
         next_value,
         _,
         _,
-    ) = collect_trajectories(envs, actor, critic, subkey, batch_size, logger)
+    ) = collect_trajectories(
+        envs, actor, critic, subkey, batch_size, logger_adapting
+    )
+    total_episodes = len(logger_adapting.get_stat("return")[0]) + batch_size
+    logger.record_stat(
+        "reward_while_adapting",
+        jnp.sum(reward).item() / total_episodes,
+        step=current_iteration,
+    )
 
     # adapt model
     optimizer_actor = nnx.Optimizer(
@@ -62,6 +73,7 @@ def inner_loop(
     )
 
     # collect trajectory with adapted model
+    logger_adapted = logger if logger is None else MemoryLogger()
     key, subkey = jax.random.split(key)
     (
         observation,
@@ -71,7 +83,15 @@ def inner_loop(
         next_value,
         _,
         _,
-    ) = collect_trajectories(envs, actor, critic, subkey, batch_size, logger)
+    ) = collect_trajectories(
+        envs, actor, critic, subkey, batch_size, logger_adapted
+    )
+    total_episodes = len(logger_adapted.get_stat("return")[0]) + batch_size
+    logger.record_stat(
+        "reward_after_adapting",
+        jnp.sum(reward).item() / total_episodes,
+        step=current_iteration,
+    )
 
     # calc loss
     advs, returns = compute_gae(
@@ -86,7 +106,8 @@ def inner_loop(
 
 
 def train_maml_ppo(
-    envs: gym.vector.VectorEnv,
+    env_set: list[gym.vector.VectorEnv],
+    task_selector: TaskSelector,
     actor: StochasticPolicyBase,
     critic: nnx.Module,
     optimizer_actor: nnx.Optimizer,
@@ -99,39 +120,42 @@ def train_maml_ppo(
     inner_critic_lr: float = 0.001,
     logger: LoggerBase | None = None,
     progress_bar: bool = True,
+    agent_name: str = "MAML_PPO",
 ) -> tuple[StochasticPolicyBase, nnx.Module, nnx.Optimizer, nnx.Optimizer]:
-    # init vars
     key = jax.random.key(seed)
-    envs = gym.wrappers.vector.RecordEpisodeStatistics(envs)
+    for i, envs in enumerate(env_set):
+        envs.reset(seed=seed)
+        env_set[i] = gym.wrappers.vector.RecordEpisodeStatistics(envs)
 
-    # loop
     for iteration in trange(iterations, disable=not progress_bar):
-        # sample env
-        # TODO sampling with vec envs
+        task_id = task_selector.select()
+        envs = env_set[task_id]
+        envs.reset()
 
-        # clone base model
         actor_clone = nnx.clone(actor)
         critic_clone = nnx.clone(critic)
 
-        # inner loop
         loss_grad_fn = nnx.value_and_grad(inner_loop, argnums=(1, 2))
         meta_loss, (grad_actor, grad_critic) = loss_grad_fn(
             envs,
             actor_clone,
             critic_clone,
             key,
-            batch_size,
-            epochs,
-            inner_actor_lr,
-            inner_critic_lr,
-            logger,
+            iteration,
+            batch_size=batch_size,
+            epochs=epochs,
+            inner_actor_lr=inner_actor_lr,
+            inner_critic_lr=inner_critic_lr,
+            logger=logger,
         )
 
-        # update meta models
         optimizer_actor.update(actor, grad_actor)
         optimizer_critic.update(critic, grad_critic)
 
+        task_selector.feedback(reward=meta_loss, policy=actor_clone)
         if logger is not None:
             logger.record_stat("meta_loss", meta_loss, step=iteration)
+            logger.record_epoch(f"{agent_name}_ACTOR", actor, step=iteration)
+            logger.record_epoch(f"{agent_name}_CRITIC", critic, step=iteration)
 
     return actor, critic, optimizer_actor, optimizer_critic
