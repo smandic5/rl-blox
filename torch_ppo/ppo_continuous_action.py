e61386904d2e10a5ce888519e15d8fbc31bdc818
Sven Mandic <51965596+smandic5@users.noreply.github.com>
2026-01-25 12:18:54 +0100

    Extracted env creation

--- torch_ppo/ppo_continuous_action.py
+++ torch_ppo/ppo_continuous_action.py
@@ -13,31 +13,7 @@ from torch.distributions.normal import Normal
 from rl_blox.logging.logger import AIMLogger, LoggerList, StandardLogger
 
 from .args import Args
-
-
-def make_env(env_id, idx, capture_video, run_name, gamma):
-    def thunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id)
-        env = gym.wrappers.FlattenObservation(
-            env
-        )  # deal with dm_control's Dict observation space
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(
-            env, lambda obs: np.clip(obs, -10, 10), env.observation_space
-        )
-        env = gym.wrappers.NormalizeReward(env, gamma=gamma)
-        env = gym.wrappers.TransformReward(
-            env, lambda reward: np.clip(reward, -10, 10)
-        )
-        return env
-
-    return thunk
+from .env_handling import make_env
 
 
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
