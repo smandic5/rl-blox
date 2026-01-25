e32d4b18486a38df813f696427dea89ff41a847d
Sven Mandic <51965596+smandic5@users.noreply.github.com>
2026-01-25 12:28:20 +0100

    Extracted Agent

--- torch_ppo/ppo_continuous_action.py
+++ torch_ppo/ppo_continuous_action.py
@@ -8,68 +8,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import tyro
-from torch.distributions.normal import Normal
 
 from rl_blox.logging.logger import AIMLogger, LoggerList, StandardLogger
 
+from .agent import Agent
 from .args import Args
 from .env_handling import make_env
-
-
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    torch.nn.init.orthogonal_(layer.weight, std)
-    torch.nn.init.constant_(layer.bias, bias_const)
-    return layer
-
-
-class Agent(nn.Module):
-    def __init__(self, envs):
-        super().__init__()
-        self.critic = nn.Sequential(
-            layer_init(
-                nn.Linear(
-                    np.array(envs.single_observation_space.shape).prod(), 64
-                )
-            ),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 1), std=1.0),
-        )
-        self.actor_mean = nn.Sequential(
-            layer_init(
-                nn.Linear(
-                    np.array(envs.single_observation_space.shape).prod(), 64
-                )
-            ),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(
-                nn.Linear(64, np.prod(envs.single_action_space.shape)), std=0.01
-            ),
-        )
-        self.actor_logstd = nn.Parameter(
-            torch.zeros(1, np.prod(envs.single_action_space.shape))
-        )
-
-    def get_value(self, x):
-        return self.critic(x)
-
-    def get_action_and_value(self, x, action=None):
-        action_mean = self.actor_mean(x)
-        action_logstd = self.actor_logstd.expand_as(action_mean)
-        action_std = torch.exp(action_logstd)
-        probs = Normal(action_mean, action_std)
-        if action is None:
-            action = probs.sample()
-        return (
-            action,
-            probs.log_prob(action).sum(1),
-            probs.entropy().sum(1),
-            self.critic(x),
-        )
-
 
 if __name__ == "__main__":
     args = tyro.cli(Args)
